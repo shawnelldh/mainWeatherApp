a62a7016efeb2fb98633a24455531d593f65af56
SHAWNELL D HARRISON <54282973+shawnelldh@users.noreply.github.com>
2020-04-22 16:01:32 -0400

    Add files via upload

---
+++ src/components/Titles.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Titles() {
+return (
+  <div>
+
+     <h1 className="title-container_title">Dat Weather,Playa </h1>
+     <p className="title-container_subtitle">
+       It May Rain On Your Head
+      </p>
+     </div>
+);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
